28140fe7a4107c3efa8414f711bcacf6e35bccfa
Renasha Rachael <renasha.cassar@my.uwi.edu>
2019-07-29 20:21:27 -0400

    added logout button

--- app/src/main/java/com/example/ttcommute/HomePageActivity.java
+++ app/src/main/java/com/example/ttcommute/HomePageActivity.java
@@ -15,6 +15,16 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -40,6 +50,9 @@ public class HomePageActivity extends AppCompatActivity implements OnMapReadyCal
     private final List<List<Point>> points = new ArrayList<>();
     private final List<Point> outerPoints = new ArrayList<>();
     private MapView mapView;
+    private Button logout;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +62,23 @@ public class HomePageActivity extends AppCompatActivity implements OnMapReadyCal
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() ==null){
+                    startActivity(new Intent(HomePageActivity.this, MainActivity.class));
+                }
+            }
+        };
+
+        logout = (Button)findViewById(R.id.button_logout);
+
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mAuth.signOut();
     }
 
     @Override
@@ -112,5 +142,11 @@ public class HomePageActivity extends AppCompatActivity implements OnMapReadyCal
         mapView.onSaveInstanceState(outState);
 
     }
+            @Override
+            protected void onStart(){
+                super.onStart();
+
+                mAuth.addAuthStateListener(mAuthListener);
+            }
 
 }
