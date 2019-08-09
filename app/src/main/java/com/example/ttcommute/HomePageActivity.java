907a093ed9f3fb351ee68cf19879e4b019e14b4b
Christopher_Ash <christopher.ash@my.uwi.edu>
2019-08-08 22:35:59 -0400

    Initial commit of basic navigation app with login (using mapbbox).

--- app/src/main/java/com/example/ttcommute/HomePageActivity.java
+++ app/src/main/java/com/example/ttcommute/HomePageActivity.java
@@ -2,12 +2,25 @@ package com.example.ttcommute;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
 import androidx.annotation.NonNull;
 
+//added for navigation
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import androidx.annotation.Nullable;
+import android.os.Parcelable;
+import android.widget.Toast;
+import android.content.Intent;
 
-import android.os.Bundle;
+//--------------------------
 
-
+// Map initialization classes
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -15,33 +28,84 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+// Location component classes
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
 
-import android.content.Intent;
+// Marker addition classes
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.picker.PlacePicker;
+import com.mapbox.mapboxsdk.plugins.places.picker.model.PlacePickerOptions;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+
+// Route calculation classes
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import android.util.Log;
+
+
+// Navigation UI classes
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
 
-
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 
 /**
  * Restrict the map camera to certain bounds.
  */
-public class HomePageActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class HomePageActivity extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener,
+        PermissionsListener{
+    // variables for adding location layer
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    // variables for adding location layer
+    private PermissionsManager permissionsManager;
+    private LocationComponent locationComponent;
+    // variables for calculating and drawing a route
+    private DirectionsRoute currentRoute;
+    private static final String TAG = "DirectionsActivity";
+    private NavigationMapRoute navigationMapRoute;
+    // variables needed to initialize navigation
+    private FloatingActionButton button;
+    //variables for search
+    private FloatingActionButton search;
+    private static final int REQUEST_CODE_AUTOCOMPLETE =1;
+    //variable to filter country in search
+    private List<String> countries = new ArrayList<>();
+    //place picker variable
+    private static final int PLACE_SELECTION_REQUEST_CODE = 56789;
 
-    private static final LatLng BOUND_CORNER_NW = new LatLng(10.6, -60.9); //latitude is bottom longitude is right
-    private static final LatLng BOUND_CORNER_SE = new LatLng(10.9, -61.55); //lattitude is top and longitude is bottom
+
+    //variables for map restriction
+    private static final LatLng BOUND_CORNER_NW = new LatLng(10, -60); //latitude is bottom longitude is right
+    private static final LatLng BOUND_CORNER_SE = new LatLng(11, -62); //lattitude is top and longitude is bottom
     private static final LatLngBounds RESTRICTED_BOUNDS_AREA = new LatLngBounds.Builder()
             .include(BOUND_CORNER_NW)
             .include(BOUND_CORNER_SE)
@@ -49,7 +113,7 @@ public class HomePageActivity extends AppCompatActivity implements OnMapReadyCal
 
     private final List<List<Point>> points = new ArrayList<>();
     private final List<Point> outerPoints = new ArrayList<>();
-    private MapView mapView;
+
     private Button logout;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -86,21 +150,210 @@ public class HomePageActivity extends AppCompatActivity implements OnMapReadyCal
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+        this.mapboxMap = mapboxMap;
+
+        //Set the boundary area for the map area
+        mapboxMap.setLatLngBoundsForCameraTarget(RESTRICTED_BOUNDS_AREA);
+
+        //Set the minimum zoom level of the map camera
+        mapboxMap.setMinZoomPreference(8);
+
+
+
+        mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) { // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                enableLocationComponent(style);
+
+                addDestinationIconSymbolLayer(style);
+
+                mapboxMap.addOnMapClickListener(HomePageActivity.this);
+                search = findViewById(R.id.searchbutton);
+                search.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        searchActivity();
+                    }
+                });
 
 
-                //Set the boundary area for the map camera
-                mapboxMap.setLatLngBoundsForCameraTarget(RESTRICTED_BOUNDS_AREA);
-
-                //Set the minimum zoom level of the map camera
-                mapboxMap.setMinZoomPreference(2);
-
+                button = findViewById(R.id.navbutton);
+                button.setEnabled(false);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean simulateRoute = false;
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(currentRoute)
+                                .shouldSimulateRoute(simulateRoute)
+                                .build();
+                        // Call this method with Context from within an Activity
+                        NavigationLauncher.startNavigation(HomePageActivity.this, options);
+                    }
+                });
 
             }
         });
     }
+
+    //method to add destination layer
+    private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addImage("destination-icon-id",
+                BitmapFactory.decodeResource(this.getResources(), R.drawable.mapbox_marker_icon_default));
+        GeoJsonSource geoJsonSource = new GeoJsonSource("destination-source-id");
+        loadedMapStyle.addSource(geoJsonSource);
+        SymbolLayer destinationSymbolLayer = new SymbolLayer("destination-symbol-layer-id", "destination-source-id");
+        destinationSymbolLayer.withProperties(
+                iconImage("destination-icon-id"),
+                iconAllowOverlap(true),
+                iconIgnorePlacement(true)
+        );
+        loadedMapStyle.addLayer(destinationSymbolLayer);
+    }
+
+    //method to drop marker and draw route when map is clicked
+    @SuppressWarnings( {"MissingPermission"})
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        Intent intent = new PlacePicker.IntentBuilder()
+                .accessToken(Mapbox.getAccessToken())
+                .placeOptions(
+                        PlacePickerOptions.builder()
+                                .statingCameraPosition(
+                                        new CameraPosition.Builder()
+                                                .target(point)
+                                                .zoom(16)
+                                                .build())
+                                .build())
+                .build(this);
+        startActivityForResult(intent, PLACE_SELECTION_REQUEST_CODE);
+
+
+
+        Point destinationPoint = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                locationComponent.getLastKnownLocation().getLatitude());
+
+
+        GeoJsonSource source = mapboxMap.getStyle().getSourceAs("destination-source-id");
+        if (source != null) {
+            source.setGeoJson(Feature.fromGeometry(destinationPoint));
+        }
+
+        getRoute(originPoint, destinationPoint);
+        button.setEnabled(true);
+        button.setBackgroundResource(R.color.mapboxBlue);
+        return true;
+    }
+
+    //method to calculate route between user location and marker
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute.removeRoute();
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
+
+    //method to enable the location component. This enables the user's current location to be seen on th map via a puck
+    //It checks user permissions with respect to the access of user location as well
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Activate the MapboxMap LocationComponent to show user location
+            // Adding in LocationComponentOptions is also an optional parameter
+            locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this, loadedMapStyle);
+            locationComponent.setLocationComponentEnabled(true);
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationComponent(mapboxMap.getStyle());
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+
+    //method enabling search with
+    private void searchActivity(){
+
+        Intent intent = new PlaceAutocomplete.IntentBuilder()
+                .accessToken(Mapbox.getAccessToken())
+                //.placeOptions(placeOptions)
+                .build(this);
+        startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+
+    }
+
+    //activity result for location search and picker
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            CarmenFeature feature = PlaceAutocomplete.getPlace(data);
+            Toast.makeText(this, feature.text(), Toast.LENGTH_LONG).show();
+        } else {
+            if (requestCode == PLACE_SELECTION_REQUEST_CODE && resultCode == RESULT_OK) {
+
+                // Retrieve the information from the selected location's CarmenFeature
+
+                CarmenFeature carmenFeature = PlacePicker.getPlace(data);
+            }
+        }
+    }
+
+
 
 
 
